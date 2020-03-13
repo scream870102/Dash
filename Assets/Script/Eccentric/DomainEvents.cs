6c8e07f1a05902785e8d1a9cf25bf3b839f3d0a6
Scream <scream870102@gmail.com>
2020-03-13 12:18:12 +0800

    Update to 19.3.4

--- Assets/Script/Eccentric/DomainEvents.cs
+++ Assets/Script/Eccentric/DomainEvents.cs
@@ -2,10 +2,9 @@
     using System.Collections.Generic;
     using System.Linq;
     using System;
-
     static class DomainEvents {
         private static Dictionary<Type, List<Delegate>> m_actionsByType;
-        public static void Register<T> (Action<T> callback)where T : IDomainEvent {
+        public static void Register<T> (Action<T> callback) where T : IDomainEvent {
             var eventType = typeof (T);
             if (m_actionsByType == null) {
                 m_actionsByType = new Dictionary<Type, List<Delegate>> ( );
@@ -13,18 +12,18 @@
             if (!m_actionsByType.ContainsKey (eventType)) {
                 m_actionsByType.Add (eventType, new List<Delegate> ( ));
             }
-            var actions = m_actionsByType [eventType];
+            var actions = m_actionsByType[eventType];
             if (!actions.Contains (callback))
                 actions.Add (callback);
         }
 
-        public static void UnRegister<T> (Action<T> callback)where T : IDomainEvent {
+        public static void UnRegister<T> (Action<T> callback) where T : IDomainEvent {
             if (m_actionsByType == null) {
                 return;
             }
             var eventType = typeof (T);
             if (m_actionsByType.ContainsKey (eventType)) {
-                var actions = m_actionsByType [eventType];
+                var actions = m_actionsByType[eventType];
                 actions.Remove (callback);
             }
         }
@@ -34,11 +33,11 @@
                 m_actionsByType = null;
             }
         }
-        public static void Raise<T> (T args)where T : IDomainEvent {
+        public static void Raise<T> (T args) where T : IDomainEvent {
             if (m_actionsByType != null) {
                 Type eventType = typeof (T);
                 if (m_actionsByType.ContainsKey (eventType)) {
-                    List<Delegate> actions = m_actionsByType [eventType];
+                    List<Delegate> actions = m_actionsByType[eventType];
                     foreach (var action in actions.Cast<Action<T>> ( ).ToList ( )) {
                         action (args);
                     }
